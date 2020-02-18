2cb36a3128d11878ab02937b7d91de250dcfdda5
Dagnos <40351219+Dagnos@users.noreply.github.com>
2020-02-18 08:35:12 -0500

    update
    ++

--- routes/ordentrabajo.js
+++ routes/ordentrabajo.js
@@ -25,4 +25,13 @@ router.get('/busqueda/:id', async (req, res) => {
   }
 })
 
+// Add a new user
+router.post('/users', (request, response) => {
+  pool.query('INSERT INTO users SET ?', request.body, (error, result) => {
+      if (error) throw error;
+
+      response.status(201).send(`User added with ID: ${result.insertId}`);
+  });
+});
+
 module.exports = router;
